9de97b7f58d87f652e58a0932c3f257a5d986154
Nirajan <nirajanrk@gmail.com>
2012-09-01 01:33:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Credo::Application.config.secret_token = 'b0dfebbee543e5cb32ef37e871912c419e9c0f7b8dc517d887a1bab5e5f5c5275db0d0c9523fedd0bce2a661635e7a81bdf417e6bb145a586399a86f475ab5fe'
