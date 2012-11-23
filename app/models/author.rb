36459c834cf1c78bb74150564eb1bce0c9f0d6c3
Richard Lee <rlee@thoughtworks.com>
2012-11-23 15:35:12 -0600

    Use custom inline error messages instead

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
   has_and_belongs_to_many :sources
-  validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name
+  validates_presence_of :last_name
   attr_accessible :first_name, :last_name, :middle_name, :authors_attributes
 end
