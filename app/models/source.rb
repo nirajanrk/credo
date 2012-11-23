a1758cad5e39d5ef47a204df463fc06bc971084b
Richard Lee <rlee@thoughtworks.com>
2012-11-23 14:29:08 -0600

    Dont save authors to source unless it has both first and last names

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,7 +1,7 @@
 class Source < ActiveRecord::Base
 
-  has_and_belongs_to_many:authors
-  accepts_nested_attributes_for :authors, :allow_destroy => true, :reject_if => lambda { |attributes| attributes['first_name'].blank? && attributes['last_name'].blank? }
+  has_and_belongs_to_many :authors
+  accepts_nested_attributes_for :authors, :allow_destroy => true, :reject_if => lambda { |attributes| attributes['first_name'].blank? || attributes['last_name'].blank? }
 
   attr_accessible :citation, :description, :journal, :title, :type, :url, :year
   attr_accessible :authors_attributes
